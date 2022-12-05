1ba17877024aee712919bf52004c5a97f2947ad8
Tony Ferrell <tferrell@dropbox.com>
2022-12-04 23:17:46 -0800

    Board parsing working, but untested.

---
+++ day5/src/main.rs
@@ -0,0 +1,102 @@
+use std::fmt::Debug;
+
+mod aoc_file;
+
+fn main() {
+    read_input_file(&aoc_file::get_file_param());
+}
+
+fn read_input_file(filename: &str) -> Game {
+    let lines = aoc_file::read_lines(filename).expect("please only give us files that exist.");
+    let board: Vec<Vec<_>> = lines
+        .iter()
+        .take_while(|x| !x.is_empty())
+        .map(|l| l.chars().collect::<Vec<_>>())
+        .collect();
+    let instructions: Vec<_> = lines.iter().skip_while(|x| !x.is_empty()).skip(1).collect();
+
+    // Game board with no columns, we'll handle that as we add them.
+    let mut game_board = Game { stacks: vec![] };
+
+    for line in board {
+        let parse_board = line 
+            .chunks(4)
+            .map(|col_val| col_val.get(1).unwrap())
+            .enumerate()
+            .filter(|(_, v)| !v.is_whitespace());
+
+        for (column, value) in parse_board {
+            game_board.add(*value, column + 1);
+        }
+    }
+
+    println!("Instructions:");
+    for instruction in instructions {
+        println!("{}", instruction);
+    }
+
+   game_board 
+}
+
+#[non_exhaustive]
+struct Game {
+    stacks: Vec<Vec<char>>,
+}
+
+impl Debug for Game {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Game")
+            .field("stacks", &self.stacks)
+            .finish()
+    }
+}
+
+impl Game {
+    fn add(&mut self, val: char, col: usize) {
+        while self.stacks.len() < col {
+            self.stacks.push(vec![]);
+        }
+
+        // Get columns back to indexes.
+        let col = col - 1;
+
+        self.stacks
+            .get_mut(col)
+            .expect("adding failed because the column is missing")
+            .push(val);
+    }
+
+    fn make_move(&mut self, source: usize, dest: usize) -> Result<(), String> {
+        let size = self.stacks.len();
+        if source < 1 || source > size {
+            return Err(format!("Source range out of bounds: {}", source));
+        } else if dest < 1 || dest > size {
+            return Err(format!("Dest range out of bounds: {}", source));
+        }
+
+        let source = source - 1;
+        let dest = dest - 1;
+
+        let source_value = dbg!(self
+            .stacks
+            .get_mut(source)
+            .expect("destination column should exist")
+            .pop()
+            .expect("destination column should not be empty"));
+        self.stacks
+            .get_mut(dest)
+            .expect("source column should exist")
+            .push(source_value);
+
+        Ok(())
+    }
+}
+
+#[test]
+fn parse_board_test() {
+    let game = dbg!(read_input_file("./test1.txt"));
+    println!("{:#?}", game);
+
+    // Write some real asserts here
+    todo!();
+}
